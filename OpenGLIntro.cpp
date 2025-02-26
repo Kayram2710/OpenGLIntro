6dd7f687005c6913cbe4d08f76d810951eea2bd5
Karim <kimomomo@hotmail.com>
2025-02-26 15:39:22 -0500

    Step 1 completed
    Followed the following tutorial: https://www.opengl-tutorial.org/beginners-tutorials/tutorial-1-opening-a-window/

--- OpenGLIntro.cpp
+++ OpenGLIntro.cpp
@@ -1,21 +1,65 @@
 // OpenGLIntro.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
 
 #include <iostream>
+#include <GL/glew.h>    
+#include <GLFW/glfw3.h>  
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    //Initializing GLFW
+    if (!glfwInit()){
+        std::cerr << "Failed to initialize GLFW." << std::endl;
+        return -1;
+    }
+
+    //GLFW Initialized. Adjusting context window.
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3); //Version 3.3
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3); //Version 3.3
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE); //Not old openGL
+    //glfwWindowHint(GLFW_RESIZABLE, GL_FALSE); //Uncomment to make window unresizable
+
+    //Create GLFW window
+    GLFWwindow* window = glfwCreateWindow(1024, 768, "OpenGLIntro", nullptr, nullptr);
+
+    //Error handling
+    if (window == NULL) {
+        std::cerr << "Failed to open GLFW window." << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+
+    //Initialize GLEW
+    glfwMakeContextCurrent(window);
+
+    //Error handling and intiate
+    GLenum glewStatus = glewInit();
+    if (glewStatus != GLEW_OK) {
+        std::cerr << "Failed to initialize GLEW." << std::endl;
+        return -1;
+    }
+
+    //Set up capturing exit key: "ESC"
+    glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
+
+    do {
+        //Clear screen
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        //Drawing Area
+
+        // Swap buffers
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+
+    } //Check if exit key is pressed
+    while (glfwGetKey(window, GLFW_KEY_ESCAPE) != GLFW_PRESS &&
+        glfwWindowShouldClose(window) == 0);
+
+    //Clean up and exit
+    glfwDestroyWindow(window);
+    glfwTerminate();
+    return 0;
+
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
     
